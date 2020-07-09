001418cdf5e23502076931c4b3b03df95dbefe78
SpandanaPalakurthy <50132713+SpandanaPalakurthy@users.noreply.github.com>
2020-07-09 17:45:05 +0530

    updated code

--- food-delivery-system/src/main/java/com/cg/fooddelivery/FoodDeliveryApplication.java
+++ food-delivery-system/src/main/java/com/cg/fooddelivery/FoodDeliveryApplication.java
@@ -1,10 +1,12 @@
 package com.cg.fooddelivery;
-
+import org.springframework.boot.SpringBootVersion; 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class FoodDeliveryApplication {
+public class FoodDeliveryApplication  extends SpringBootServletInitializer  {
 
 	public static void main(String[] args) {
 		SpringApplication.run(FoodDeliveryApplication.class, args);
@@ -13,4 +15,11 @@ public class FoodDeliveryApplication {
 
 	}
 
+	@Override 
+
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) { 
+
+	return builder.sources(FoodDeliveryApplication.class); 
+
+	} 
 }
